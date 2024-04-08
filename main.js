555fdb1a784326dd8d62453964a8c300bbff51cd
foddom520 <115153461+foddom520@users.noreply.github.com>
2024-04-08 22:09:49 +0200

    asder

---
+++ main.js
@@ -0,0 +1,12 @@
+function OnClickButton() {
+    console.log("I like cows")
+}
+function OnChangeInput() {
+    console.log(document.getElementById('ID').value)
+}
+function OnInputInput() {
+    console.log(document.getElementById('ID').value)
+}
+function OnDbClickButton() {
+    console.log("I like cows")
+}
