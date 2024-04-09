ea802952d3b3b250445facd606e9a676ffcf7e90
foddom520 <115153461+foddom520@users.noreply.github.com>
2024-04-09 21:30:13 +0200

    index

---
+++ buttonJS.js
@@ -0,0 +1,89 @@
+const toastTrigger1 = document.getElementById('liveToastBtn')
+const toastLiveExample1 = document.getElementById('liveToast')
+if (toastTrigger1) {
+  toastTrigger1.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample1)
+
+    toast.show()
+  })
+}
+
+const toastTrigger2 = document.getElementById('SusieStinky')
+const toastLiveExample2 = document.getElementById('Susie')
+if (toastTrigger2) {
+  toastTrigger2.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample2)
+
+    toast.show()
+  })
+}
+
+const toastTrigger3 = document.getElementById('Sammy')
+const toastLiveExample3 = document.getElementById('SammyGay')
+if (toastTrigger3) {
+  toastTrigger3.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample3)
+
+    toast.show()
+  })
+}
+
+const toastTrigger4 = document.getElementById('Time')
+const toastLiveExample4 = document.getElementById('TimeIsSus')
+if (toastTrigger4) {
+  toastTrigger4.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample4)
+
+    toast.show()
+  })
+}
+
+const toastTrigger5 = document.getElementById('Flower')
+const toastLiveExample5 = document.getElementById('FlowerIsFriday13')
+if (toastTrigger5) {
+  toastTrigger5.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample5)
+
+    toast.show()
+  })
+}
+
+const toastTrigger6 = document.getElementById('Bobby')
+const toastLiveExample6 = document.getElementById('BobbyIsAGhost')
+if (toastTrigger6) {
+  toastTrigger6.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample6)
+
+    toast.show()
+  })
+}
+
+const toastTrigger7 = document.getElementById('Joran')
+const toastLiveExample7 = document.getElementById('JoranIsABaby')
+if (toastTrigger7) {
+  toastTrigger7.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample7)
+
+    toast.show()
+  })
+}
+
+const toastTrigger8 = document.getElementById('demon')
+const toastLiveExample8 = document.getElementById('Demonsip')
+if (toastTrigger8) {
+  toastTrigger8.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample8)
+
+    toast.show()
+  })
+}
+
+const toastTrigger9 = document.getElementById('Anna')
+const toastLiveExample9 = document.getElementById('AnnaIsNice')
+if (toastTrigger9) {
+  toastTrigger9.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample9)
+
+    toast.show()
+  })
+}
